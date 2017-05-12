be70c4e1539ead0eb3af69e922f657adb6701f69
jagluck <jagluck@terpmail.umd.edu>
2017-05-12 00:28:06 -0400

    orphan and sterile

--- report.php
+++ report.php
@@ -13,19 +13,19 @@
 	<h1 class="centered">DAGR Database Reports</h1>
 	
 	<h2>Orphan Report</h2>
-	<form>
+	<form action="report_result.php"  method="get">
 		<input name="report_type" value="orphan" type="hidden">
 		<input type="submit" value="Get Report">
 	</form>
 
 	<h2>Sterile Report</h2>
-	<form>
+	<form action="report_result.php">
 		<input name="report_type" value="sterile" type="hidden">
 		<input type="submit" value="Get Report">
 	</form>
 	
 	<h2>Reach Query</h2>
-	<form>
+	<form action="report_result.php">
 		 GUID: <input type="text" style='width:20em' name="guid" value="3270C5C5-F589-4BA2-820B-6EBD0DD4C85E">
 		<br><br>
 		<input name="report_type" value="reach" type="hidden">
@@ -33,7 +33,7 @@
 	</form>
 	
 	<h2> Time Range Report</h2>
-	<form>
+	<form action="report_result.php">
 		Format is month/day/year
 		<br><br>
 		Start time: <input type="datetime-local" name="start_time" value="<?php echo date('2000-01-01\T01:00:00.000');?>">
--- report_result.php
+++ report_result.php
@@ -1,6 +1,6 @@
 <html>
 	<head> 
-		<title> DAGR Reports </title>
+		<title> Report Results </title>
 		<link rel="stylesheet" href="scripts/stylesheet.css">
 	</head>
 	<body>
@@ -9,9 +9,65 @@
 		include 'scripts/credentials.php';
 		include 'scripts/functions.php';
 		include 'scripts/navbar.php';
-	?>
+	?> 
+	
+	<h1 style="margin-left:2%"> Report Results </h1>
+	
+	<?php
+		// Create SQL connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
 
+		if(empty($_GET['report_type'])){
+			echo("Invalid report request. </body></html>");
+			exit();
+		}		
+
+		if(strcmp($_GET['report_type'], 'orphan') == 0){
+			$sql = "SELECT * FROM (SELECT * FROM dagr d LEFT JOIN children on d.id = children.child_id WHERE children.child_id IS NULL) t, metadata m WHERE m.dagr_id = t.id";
+				
+		} else if(strcmp($_GET['report_type'], 'sterile') == 0) {
+			$sql = "SELECT * FROM (SELECT * FROM dagr d LEFT JOIN children on d.id = children.parent_id WHERE children.parent_id IS NULL) t, metadata m WHERE m.dagr_id = t.id";
+		} else if(strcmp($_GET['report_type'], 'range') == 0) {
+		}
+		
+		$result = $conn->query($sql);
+		if(mysqli_num_rows($result) <= 0){
+			echo $sql;
+			echo("No records returned. Please alter your search and try again.</body></html>");
+			exit();
+		}
+		
+		
+		echo "<p>";
+		if ($result->num_rows > 0) {
+			// output data of each row
+			echo "<table>	<tr><th>Name</th> 
+								<th>Author</th>
+								<th>File path</th> 								
+								<th>File type</th>
+								<th>File size</th> 
+								<th>Time Edited</th> 
+							</tr>";
+			while($row = $result->fetch_assoc()) {
+				echo "<tr><td><a href=\"view_dagr.php?guid=".$row["id"]."\">".$row["name"]."</a>".
+					"</td><td>".$row["author"].
+					"</td><td>".$row["path"].
+					"</td><td>".$row["file_type"].
+					"</td><td>".$row["file_size"].
+					"</td><td>".$row["time_edited"].
+					"</td></tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "0 results";
+		}
+		echo "</p>";
+		$conn->close();
+	?>
 	
-	
+
 	</body>
 </html>
