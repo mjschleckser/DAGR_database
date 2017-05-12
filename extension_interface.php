b475fa68fba3b67b473cdc1a7171f516fc65f811
mjschleckser <mjschleckser@gmail.com>
2017-05-12 02:20:47 -0400

    Working on extension

---
+++ extension_interface.php
@@ -0,0 +1,20 @@
+<html>
+	<head> 
+		<title> Extension Interface </title>
+		<link rel="stylesheet" href="scripts/stylesheet.css">
+	</head>
+	<body>
+	
+	<?php 
+		include 'scripts/credentials.php';
+		include 'scripts/functions.php';
+		include 'scripts/navbar.php';
+	?>
+	
+	
+	
+	<?php
+		$conn->close();
+	?>
+	</body>
+</html>
